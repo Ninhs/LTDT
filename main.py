173d2f159f7373d23553f4506539aa0f80dac6b3
Ninhs <dangninh1107@gmail.com>
2025-12-20 14:02:54 +0700

    update layout

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ frame_graph_type = tk.LabelFrame(root, text="Loại đồ thị", bg="cyan")
 frame_graph_type.place(x=10, y=10, width=210, height=80)
 
 graph_type = tk.StringVar(value="vo_huong")
-tk.Radiobutton(frame_graph_type, text="Đồ thị vô hướng", variable=graph_type, value="vo_huong").pack(anchor="w")
-tk.Radiobutton(frame_graph_type, text="Đồ thị có hướng", variable=graph_type, value="co_huong").pack(anchor="w")
+tk.Radiobutton(frame_graph_type, text="Đồ thị vô hướng", bg="cyan", variable=graph_type, value="vo_huong").pack(anchor="w")
+tk.Radiobutton(frame_graph_type, text="Đồ thị có hướng", bg="cyan", variable=graph_type, value="co_huong").pack(anchor="w")
 
 # Khung chọn đỉnh tìm đường đi
 frame_vertices = tk.LabelFrame(root, text="Đỉnh", bg="cyan")
 frame_vertices.place(x=220, y=10, width=396, height=80)
 
-tk.Label(frame_vertices, text="Đỉnh đầu:").grid(row=0, column=0, sticky="w", padx=5, pady=5)
+tk.Label(frame_vertices, text="Đỉnh đầu:", bg="cyan").grid(row=0, column=0, sticky="w", padx=5, pady=5)
 start_vertex = ttk.Combobox(frame_vertices, values=["1","2","3","4","5"])
 start_vertex.grid(row=1, column=0, padx=5, pady=5)
 
-tk.Label(frame_vertices, text="Đỉnh đích:").grid(row=0, column=1, sticky="w", padx=5, pady=5)
+tk.Label(frame_vertices, text="Đỉnh đích:", bg="cyan").grid(row=0, column=1, sticky="w", padx=5, pady=5)
 end_vertex = ttk.Combobox(frame_vertices, values=["1","2","3","4","5"])
 end_vertex.grid(row=1, column=1, padx=5, pady=5)
 
-tk.Button(frame_vertices, text="Tìm đường đi").grid(row=1, column=2, padx=10, pady=5)
+tk.Button(frame_vertices, text="Tìm đường đi").grid(row=1, column=2, padx=1, pady=1)
 
 # Canvas vẽ đồ thị
 canvas = tk.Canvas(root, bg="white", width=678, height=400)
@@ -38,10 +38,10 @@ canvas.place(x=10, y=90)
 frame_right = tk.Frame(root, bg="cyan")
 frame_right.place(x=617, y=10, width=375, height=80)
 
-frame_traverse = tk.LabelFrame(frame_right, text="Duyệt đồ thị")
-frame_traverse.pack(fill="x", padx=5, pady=5)
+frame_traverse = tk.LabelFrame(frame_right, text="Duyệt đồ thị", bg="cyan")
+frame_traverse.pack(fill="x", padx=5, pady=5, ipady=10, ipadx=10)
 
-ttk.Label(frame_traverse, text="Loại thuật toán:").grid(row=0, column=0, padx=5, pady=5, sticky="w")
+tk.Label(frame_traverse, text="Loại thuật toán:", bg="cyan").grid(row=0, column=0, padx=5, pady=5, sticky="w")
 cb_start = ttk.Combobox(frame_traverse,
                         values=["DFS", "BFS", "Bellman-Ford", "Dijkstra", "Prim", "Kruskal", "Sequential Color"],
                         state="readonly",
@@ -93,7 +93,7 @@ entry_weight.grid(row=2, column=1, sticky='ew', padx=(0, 10), pady=8)
 
 # Nút Thêm cạnh
 btn_add_edge = tk.Button(frame_add, text="Thêm cạnh", width=25, bg="lightgreen")
-btn_add_edge.grid(row=3, column=0, columnspan=3,sticky="we", padx=5, pady=35)
+btn_add_edge.grid(row=3, column=0, columnspan=3,sticky="we", padx=5, pady=25, ipady=10)
 
 
 controller = GraphController(
@@ -110,7 +110,7 @@ controller = GraphController(
 # Bind nút "Thêm cạnh" để gọi hàm add_edge trong controller
 btn_add_edge.config(command=controller.add_edge)
 
-tk.Button(frame_add, text="Thêm đỉnh", command=controller.enable_add_vertex).grid(row=4, column=0, columnspan=3, sticky="we", padx=5, pady=45)
+tk.Button(frame_add, text="Thêm đỉnh", command=controller.enable_add_vertex).grid(row=4, column=0, columnspan=3, sticky="we", padx=5, pady=35, ipady=10)
 tk.Button(frame_add, text="Cập nhật", padx=20, pady=8).grid(row=5, column=0, padx=(0,5), sticky="we")
 tk.Button(frame_add, text="Di chuyển", padx=20, pady=8).grid(row=5, column=1, padx=(0, 5), sticky="we")
 tk.Button(frame_add, text="Làm mới", padx=20, pady=8).grid(row=5, column=2, sticky="we")
